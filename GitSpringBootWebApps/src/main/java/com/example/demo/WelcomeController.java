6bd8101105850bbbe8f4fc925c309dc4c449d1f3
porus <porus@porus-HP-Laptop-15-bs1xx>
2020-04-22 16:15:56 -0400

    New Endpoints added in WelcomeController

--- GitSpringBootWebApps/src/main/java/com/example/demo/WelcomeController.java
+++ GitSpringBootWebApps/src/main/java/com/example/demo/WelcomeController.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import javax.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +14,12 @@ public class WelcomeController {
 	public String getMessage()
 	{
 		return "<h1>Welcome to Spring</h1>";
+	}
+	
+	@RequestMapping("/user/{name}")
+	public String getUserMessage(@PathVariable(name ="name") String name)
+	{
+		return "<h1>Welcome to <font color='green'>"+name+"</font></h1>";
 	}
 
 }
